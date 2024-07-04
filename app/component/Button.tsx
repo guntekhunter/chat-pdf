370f318ac0d16c687f25e71106979d8f10b27fb6
guntekhunter <guntek2000@gmail.com>
2024-07-04 23:20:29 +0800

    fead: new ui

--- app/component/Button.tsx
+++ app/component/Button.tsx
@@ -4,7 +4,7 @@ export default function Button(props: any) {
   return (
     <button
       onClick={props.onClick}
-      className={`rounded-md bg-[#0B1215] py-[.3rem] px-[2rem] text-white ${props.className} `}
+      className={`rounded-md bg-[#0B1215] py-[.3rem] px-[2rem] text-white ${props.className} w-full`}
     >
       {props.children}
     </button>
--- app/upload-pdf/page.tsx
+++ app/upload-pdf/page.tsx
@@ -6,64 +6,14 @@ import Button from "../component/Button";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-interface PdfResponse {
-  response: Array<{
-    text: string;
-    vectors: number[];
-  }>;
-}
-
 export default function page() {
-  const [pdfData, setPdfData] = useState();
   const [pdfUpload, setPdfUpload] = useState<any>();
   const [fileName, setFileName] = useState("");
-  const [embedData, setEmbedData] = useState();
-  const [vector, setVector] = useState();
-  const [input, setInput] = useState("");
-  const [file, setFile] = useState<File>();
-  const [inputData, setInputData] = useState<string | ArrayBuffer | null>(null);
-
-  const route = useRouter();
-
-  const fetchPdf = async () => {
-    const res = await axios.get("/api/pdf-parse");
-    setPdfData(res.data.response);
-    return res.data.response;
-  };
-  const fetchVectors = async (pdfContent: any) => {
-    const dataPdf = await fetchPdf();
-    const res = await axios.post("/api/embed", { pdf: pdfContent });
-    console.log(res.data);
-    setEmbedData(res.data.response);
-    setVector(res.data);
-    return res.data.response;
-  };
-  const createTable = async () => {
-    const data1 = await fetchPdf();
-    const data2 = await fetchVectors(data1);
-    console.log(data1);
-    console.log(data2);
-    const res = await axios.post("/api/post-supabase", {
-      pdf: pdfUpload,
-      vector: data2,
-    });
-    console.log(res.data);
-  };
-
-  const compare = async () => {
-    const res = await axios.post("/api/compare", {
-      input,
-      vector: embedData,
-    });
-    console.log("vector chatnya", res.data.response);
-    return res;
-  };
 
   const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
       setFileName(file.name);
-      setFile(e.target.files[0]);
       console.log("Selected file:", file);
       const formData = new FormData();
       formData.append("file", file);
@@ -73,21 +23,8 @@ export default function page() {
         },
       });
       const data = await vectorizeChunks(res.data.chunks);
-      console.log(data);
       setPdfUpload(data);
     }
-  };
-
-  const createParse = async () => {
-    console.log(file);
-    if (!file) {
-      console.error("No file selected");
-      return;
-    }
-    const formData = new FormData();
-    formData.append("file", file);
-    const res = uploadPdf(file);
-    console.log(res);
   };
 
   const uploadToSupabase = async () => {
@@ -103,27 +40,48 @@ export default function page() {
     }
   };
   return (
-    <div className="bg-[#F8F8F8] w-full flex justify-center py-[2rem] text-[#0B1215] h-[100vh]">
-      <div className="w-[70%] bg-white rounded-md px-[3rem] py-[4rem] text-center flex justify-center align-center">
-        <div className="w-[60%] space-y-[1rem]">
-          <h1 className="font-bold text-[2rem]">Masukkan PDF Mu</h1>
-          <div className="w-full h-[10rem] rounded-[1rem] border-dashed border-[2px] flex items-center justify-center relative">
-            <input
-              type="file"
-              onChange={handleFileChange}
-              className="absolute opacity-0 w-full h-full cursor-pointer bg-black"
-            />
-            {fileName ? (
-              <div className="bg-black text-white p-2 rounded flex justify-center content-center">
-                {fileName}
-              </div>
-            ) : (
-              <div className="bg-black text-white p-2 rounded flex justify-center content-center">
-                Drop Atau Klik Disini
-              </div>
-            )}
+    <div className="bg-red-200 flex text-[.8rem] h-[100vh]">
+      <div className="w-[20%] bg-[#F8F8F8] p-[1rem] space-y-[1rem]">
+        <div className="px-[1rem]">
+          <p className="text-[#949494]">Fitur Lainnya</p>
+        </div>
+        <div className="px-[1rem]">
+          <p className="text-black font-medium">Buat Daftar Pustaka</p>
+        </div>
+        <div className="bg-[#ECECEC] px-[1rem] py-[.5rem] rounded-md">
+          <p className="text-black font-medium">Chat PDF</p>
+        </div>
+        <div className="w-full h-[5rem] rounded-[1rem] border-dashed border-[2px] flex items-center justify-center relative">
+          <input
+            type="file"
+            onChange={handleFileChange}
+            className="absolute opacity-0 w-full h-full cursor-pointer"
+          />
+          {fileName ? (
+            <div className="text-black font-medium p-2 rounded flex justify-center content-center">
+              {fileName}
+            </div>
+          ) : (
+            <div className="text-black font-medium p-2 rounded flex justify-center content-center">
+              Drop File Atau Klik Disini
+            </div>
+          )}
+        </div>
+        <Button onClick={uploadToSupabase} className="w-full">
+          Mulai Chat
+        </Button>
+      </div>
+      <div className="w-[40%] bg-yellow-200">dua</div>
+      <div className="w-[40%] bg-white py-[2rem] flex flex-col ">
+        <div className="flex-grow"></div>
+        <div className="bg-[#F4F4F4] px-[1rem] h-[2.5rem] flex rounded-full mx-[1.5rem] relative ">
+          <input
+            type="text"
+            className="w-full py-[.5rem] px-[.5rem] bg-[#F4F4F4] focus:outline-none focus:ring-0"
+          />
+          <div className="flex justify-center items-center h-full absolute right-[.5rem]">
+            <div className="bg-[#D9D9D9] w-[1.5rem] h-[1.5rem] rounded-full"></div>
           </div>
-          <Button onClick={uploadToSupabase}>Mulai Chat</Button>
         </div>
       </div>
     </div>
