d7d8d17c4a889e8d32918ecf87e10c0c6d85fe19
jundi469 <saidaljundi8@gmail.com>
2024-07-11 15:58:41 +0700

    fead: chat functionality

--- app/api/function/groq/Groq.tsx
+++ app/api/function/groq/Groq.tsx
@@ -12,17 +12,16 @@ const fetchData = (
   return new Promise(async (resolve, reject) => {
     try {
       //create prompt
-      const promptTemplate = `kamu adalah asisten ai yang mengetahui segalanya
-    
-    Context section: ${context} pertanyaan: """${inputText}"""`;
-      // ask ai using groq api
-
+      const promptTemplate = `Kamu adalah asisten AI yang akan menjawab pertanyaan berdasarkan konteks yang diberikan. Jika kamu kurang mengerti dengan konteksnya, minta informasi yang lebih jelas. Buat semua responmu dalam bentuk markup language.\n\nContext section: ${context}\n\nPertanyaan: "${inputText}"`;
       const model = new ChatGroq({
-        apiKey: "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",
+        apiKey: "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",
       });
 
       const prompt = ChatPromptTemplate.fromMessages([
-        ["system", "You are a helpful assistant"],
+        [
+          "system",
+          "Kamu adalah asisten yang bisa menjawab semua pertanyaan berdasarkan context yang diberikan. Buat semua responsemu dalam bahasa Indonesia",
+        ],
         ["human", promptTemplate],
       ]);
 
